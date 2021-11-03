50a50327f8a39451be7b894c69295aed5adc8681
konigin144 <kinga777s@gmail.com>
2021-11-03 09:53:53 +0100

    changes

--- lab1/main.py
+++ lab1/main.py
@@ -1,38 +1,46 @@
 import math, sys
 
 def readFile(filename):
-    charNums = dict()
-    num = 0
+    charCount = dict()
+    charInf = dict()
+    totalCount = 0
     with open(filename, encoding='utf-8') as f:
         while True:
             c = f.read(1)
             if not c:
                 break
-            num += 1
-            if c in charNums:
-                charNums[c] += 1
+            totalCount += 1
+            if c in charCount:
+                charCount[c] += 1
             else:
-                charNums[c] = 1
+                charCount[c] = 1
 
-    return charNums, num
+    charCount_sorted = {k: v for k, v in sorted(charCount.items(), key=lambda item: item[1], reverse=True)}
+    
+    for char, count in charCount_sorted.items():
+        pe = count/totalCount
+        charInf[char] = math.log2(1/pe)
+    
+    return charCount_sorted, charInf, totalCount
 
 def entropy(dict, num):
     entropy = 0
     for key, value in dict.items():
         pe = value/num
-        ie = math.log2(1/pe) * 8
+        ie = math.log2(1/pe)
         entropy += pe * ie
     return entropy, ie
 
 def main():
     for arg in sys.argv:
-        dict, num = readFile(arg)
-        ent, ie = entropy(dict, num)
+        charCount, charInf, num = readFile(arg)
+        ent, ie = entropy(charCount, num)
 
         print(arg)
-        for key, value in dict.items():
-            print(str(key)+" : "+str(value)+"\t:"+str())
-        print("Liczba znaków: "+str(num))
+        print("Znak : liczba wystąpień : entropia")
+        for key, value in charCount.items():
+            print(str(key)+" : "+str(value)+" : "+str(charInf[key]))
+        print("\nLiczba znaków: "+str(num))
         print("Entropia: "+str(ent))
         print("Ilość informacji: "+str(ie))
         print()
